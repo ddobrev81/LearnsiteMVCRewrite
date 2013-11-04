cf594aa7ee5dea927c3e3a60d799ae7392a85965
Lawrence MacGuire <leprechaun@gmail.com>
2013-11-04 22:14:19 +0800

    No need for break, you're already returning

--- classes/Controller.php
+++ classes/Controller.php
@@ -23,72 +23,58 @@
             case "main":
                 include "apps/$page/$page.php";
                 return $content;
-                break;
             
             case "shoutbox":
                 include './apps/shoutbox/shoutbox.php';
                 return $content;
-                break;
             
             case "showquotes":
                 include 'apps/quotes/showquotes.php';
                 return $content;
-                break;
             
             case "addquote":
                 include './apps/quotes/addquote.php';
                 return $content;
-                break;
             
             case "underconstruction":
                 include './apps/underconstruction/underconstruction.php';
                 return $content;
-                break;
             
             case "deleteuser":
                 include './apps/users/deleteuser.php';
                 return $content;
-                break;
             
             case "edituser":
                 include './apps/users/edituser.php';
                 return $content;
-                break;
             
             case "loggedin":
                 include './apps/users/loggedin.php';
                 return $content;
-                break;
             
             case "login":
                 include './apps/users/login.php';
                 return $content;
-                break;
             
             case "logout":
                 include './apps/users/logout.php';
                 return $content;
-                break;
             
             case "changepassword":
                 include './apps/users/changepassword.php';
                 return $content;
-                break;
             
             case "register":
                 include './apps/users/register.php';
                 return $content;
-                break;
             
             case "viewusers":
                 include './apps/users/viewusers.php';
                 return $content;
-                break;
             
             default:
                 include 'apps/main/main.php';
                 return $content;
-                break;
         }
         
     }
