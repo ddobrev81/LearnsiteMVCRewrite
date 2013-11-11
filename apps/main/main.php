a0d12b86ad906ce8a33784242bf17d65d3118e29
ddobrev81 <ddobrev81@gmail.com>
2013-11-11 09:51:14 +0200

    Update main.php

--- apps/main/main.php
+++ apps/main/main.php
@@ -1,8 +1,7 @@
 <?php
 $this->cContent[] ='
-    <p>The code for this site can be found <a href="https://github.com/ddobrev81/learnsite">here.</a></p>
+    <p>The code for this site can be found <a href="https://github.com/ddobrev81">here.</a></p>
     <p>Check out that <a href="http://ddod.host22.com/fancytext">FancyText</a> plug-in!</p>
-    <p>Its <a href="http://ddod.host22.com/supersizesite">SuperSized</a></p>
 ';
 
 ?>
